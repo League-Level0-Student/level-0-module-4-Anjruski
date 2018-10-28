d135bd149f6c9e36e7d30ea5552d3c2b6ed10d2a
Anjruski <league@WorthyMini-5.attlocal.net>
2018-10-28 15:56:09 -0700

    cOdE
    :)

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -10,21 +10,51 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 String pet = JOptionPane.showInputDialog("what kind of pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 20; i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "What do you do with your" + pet, "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What do you do with your " + pet, "[insert text here]", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Pet", "Feed", "Walk" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+if (task == 0) {
+	pet();
+}
+if (task == 1) {
+	feed();
+}
+if (task == 2) {
+	walk();
+}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
+System.out.println(score);
 
+if (score == 20) {
+	break;
+}
 	}
-
+	}
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+static int score = 0;
+	static void pet() {
+JOptionPane.showMessageDialog(null , "Your pet got a happier mood." );	
+score++;
+}
+
+static void feed() {
+JOptionPane.showMessageDialog(null, "Your pet eats and then goes to sleep");
+score=score +2;
+}
+static void walk() {
+JOptionPane.showMessageDialog(null, "You and your pet for for a walk together and you are tired out at the end");
+score=score +2;
+}
+
 }
